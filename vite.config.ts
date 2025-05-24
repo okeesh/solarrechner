ab6403f5260bf53925d3754eb88b929fdae49338
okanr <okan.rn@outlook.de>
2025-05-24 12:59:00 +0200

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/', // oder anpassen wenn Unterordner
   plugins: [react(), tailwindcss()],
 })
